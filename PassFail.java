74d1b1fb01e83b71cccd83f390ca3339f70619e8
Adnan Ahmad <adnanahmad3643@gmail.com>
2025-10-06 18:49:10 +0530

    Add files via upload

---
+++ PassFail.java
@@ -0,0 +1,7 @@
+public class PassFail {
+    public static void main(String[] args) {
+        int marks=100;
+        String result= (marks>33)? "pass ":"fail";
+        System.out.println("RESULT  IS = "+result);
+    }
+}

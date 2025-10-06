c70fa90e0f3651eedf7912a17f265f4090c34840
Adnan Ahmad <adnanahmad3643@gmail.com>
2025-10-06 18:51:36 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,9 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num=80;
+        if (num%2==0)
+        System.out.println("NUMBER IS EVEN = "+num);
+        else
+        System.out.println("NUMBER IS ODD = "+num);
+    }
+}

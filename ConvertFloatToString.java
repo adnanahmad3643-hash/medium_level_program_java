059c7fc67201906afc272ccb8a7bc1629f2b0c25
Adnan Ahmad <adnanahmad3643@gmail.com>
2025-10-06 18:52:59 +0530

    Add files via upload

---
+++ ConvertFloatToString.java
@@ -0,0 +1,9 @@
+public class ConvertFloatToString {
+    // 12.	Convert float to string and back
+    public static void main(String[] args) {
+        float f=109.70f;
+        String str=Float.toString(f);
+        System.out.println(str);
+
+    }
+}
---
+++ DiffrenceDivsionIntToFloat.java
@@ -0,0 +1,9 @@
+public class DiffrenceDivsionIntToFloat {
+    // 13.	Difference between integer division and float division.
+    public static void main(String[] args) {
+        int a=100;
+        float f=109.90f;
+        System.out.println(f/10);// FLAOT DIVISION CAN MAKE ACTUAL RESULT . THAT COUNT FRACTION VALUE... 
+        System.out.println(a/10);//  INTEGER DIVISION CAN'T COUNT FRAXCTION VALUE ONLY DECIMAL VALUE COUNT. 
+    }
+}

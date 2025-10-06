286ec8a175aea9d9d5cff9e3aca389395393bed4
Adnan Ahmad <adnanahmad3643@gmail.com>
2025-10-06 18:51:14 +0530

    Add files via upload

---
+++ IncrementDecrement.java
@@ -0,0 +1,11 @@
+public class IncrementDecrement {
+    // 17.	Use increment and decrement operators in expressions
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println("ACTUAL VALUE "+a);
+        System.out.println("post Incremenentt "+a++);
+        System.out.println("pre Incremenent "+ ++a);
+          System.out.println(" Post Decremenent "+a--);
+            System.out.println(" pre Decremenent "+ --a);
+    }
+}

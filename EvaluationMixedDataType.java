c4617ce62366ece165a1c1e627504bebde642b5f
Adnan Ahmad <adnanahmad3643@gmail.com>
2025-10-06 18:52:08 +0530

    Add files via upload

---
+++ EvaluationMixedDataType.java
@@ -0,0 +1,10 @@
+public class EvaluationMixedDataType {
+    // 14.	Expression evaluation with mixed data types (int + float + double)
+    public static void main(String[] args) {
+        int num=10;
+        float f=60.2f;
+        double d=60.8;
+        double sum= num+f+d;
+        System.out.println(sum);
+    }
+}

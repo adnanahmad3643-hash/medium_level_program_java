a30dc58825c62fca9c1bca0d32b884e638129381
Adnan Ahmad <adnanahmad3643@gmail.com>
2025-10-06 18:50:31 +0530

    Add files via upload

---
+++ MaxNumber.java
@@ -0,0 +1,8 @@
+public class MaxNumber {
+    public static void main(String[] args) {
+        int first=100;
+        int second=20;
+        int result= (first>second)? first:second;
+        System.out.println("MAXIMUM NUMBER IS = "+result);
+    }
+}

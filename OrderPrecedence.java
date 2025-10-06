2b6403a8a68a8a2cf04ddfec73a04fec0516dc87
Adnan Ahmad <adnanahmad3643@gmail.com>
2025-10-06 18:49:46 +0530

    Add files via upload

---
+++ OrderPrecedence.java
@@ -0,0 +1,13 @@
+public class OrderPrecedence {
+    // 15.	Order of precedence in expressions..     
+    public static void main(String[] args) {
+        // steps:-
+        //  ()
+        // (++, --, !, -
+        //  * / %
+        // + - 
+        //  <, >, <= ,>=
+        //   && || !=
+        //  =  
+    }
+}

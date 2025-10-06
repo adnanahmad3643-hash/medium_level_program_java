fadc8a303711409d3d113758e884e4a227d257a4
Adnan Ahmad <adnanahmad3643@gmail.com>
2025-10-06 18:48:34 +0530

    Add files via upload

---
+++ TypeCastCharToInt.java
@@ -0,0 +1,9 @@
+public class TypeCastCharToInt {
+    // 10.	Convert character to int using type casting.
+    public static void main(String[] args) {
+        char ch='A';
+        int a=(int) ch;
+        System.out.println("CHARACTER VALUE = "+ch);
+        System.out.println("AFTER TYPECASTING CHAR TO INT = "+a);
+    }
+}

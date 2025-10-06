9ca9d829b88ca0864a8a084db579bd8b755827ba
Adnan Ahmad <adnanahmad3643@gmail.com>
2025-10-06 18:52:32 +0530

    Add files via upload

---
+++ DifrencePrePost.java
@@ -0,0 +1,8 @@
+public class DifrencePrePost {
+    // 18.	Difference between pre-increment and post-increment
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(a++);// postIncremenent increase the value after the Execution..
+        System.out.println(++a);//PreIncremenent Increase the value between the Execution..
+    }
+}
